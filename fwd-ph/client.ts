55f2c7fa4846f4603482b4cef7f2dedfe316a838
ssande-fwd <ssande@forwardedu.com>
2025-02-27 12:39:59 -0500

    ph init

---
+++ fwd-ph/client.ts
@@ -0,0 +1,23 @@
+namespace fwdSensors {
+
+    //% fixedInstances
+    export class FwdPhClient extends modules.AcidityClient {
+
+        constructor(role: string) {
+            super(role)
+        }
+
+        /**
+         * Returns the sensor's distance reading in meters
+         */
+        //% group="pH"
+        //% block="$this pH"
+        //% blockId=fwd_ph_get_ph
+        fwdAcidity(): number { return super.acidity() }
+
+
+    }
+
+    //% fixedInstance whenUsed
+    export const ph1 = new FwdPhClient("ph1");
+}
