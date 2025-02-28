3609eeb56d64adac6a627d0f2eee60af409b08be
ssande-fwd <ssande@forwardedu.com>
2025-02-28 15:46:11 -0500

    back to ph calibration block

--- fwd-ph/client.ts
+++ fwd-ph/client.ts
@@ -17,12 +17,16 @@ namespace fwdSensors {
 
 
         /**
-         * Returns the sensor's distance reading in meters
+         * Generates linear calibration and saves slope and y-intercept for future readings
+         * @param ph4Reading test
+         * @param ph7Reading test
          */
         //% group="pH"
-        //% block="$this pH2"
-        //% blockId=fwd_ph_get_ph2
-        fwdPh2(): number { return super.acidity() }
+        //% block="$this measures pH 4 as $ph4Reading and pH 7 as $ph4Reading"
+        //% blockId=fwd_ph_calibration
+        fwdPhCalibrate( ph4Reading: number, ph7Reading: number ): void {
+            return super.acidity()
+        }
     }
 
     //% fixedInstance whenUsed
